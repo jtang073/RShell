af2fb3225355913bc4bd1c2d1f23c21f606587f7
Jason Tang <jtang073@ucr.edu>
2019-05-25 21:34:01 -0700

    Finished Test command

--- src/Command.cpp
+++ src/Command.cpp
@@ -8,6 +8,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 using namespace std;
 
@@ -21,6 +22,75 @@ bool Command::execute() {
 	if(strcmp(arguments.at(0), "exit") == 0) {
 		exit(0);
 	}
+//////////////////////////////////////////////////
+	if (strcmp(arguments.at(0), "test") == 0 || strcmp(arguments.at(0), "[") == 0) {
+		cout << "goteem" << endl;
+		if (strcmp(arguments.at(1), "-e") == 0) {
+			struct stat temp;
+			cout << "testing -e" << endl;
+			if (stat(arguments.at(2), &temp) == 0) {
+				cout << "(True)" << endl;
+				return true;
+			}
+			else {
+				cout << "(False)" << endl;
+				return false;
+			}
+		}
+		else if (strcmp(arguments.at(1), "-f") == 0) {
+			struct stat temp;
+			cout << "testing -f" << endl;
+			if (stat(arguments.at(2), &temp) != 0) {
+//				if (temp.st_mode &S_IFREG) {
+//					cout << "(True)" << endl;
+//					return true;
+//				}
+				cout << "(False)" << endl;
+				return false;
+			}
+			else if (temp.st_mode &S_IFREG) {
+				cout << "(True)" << endl;
+				return true;
+			}
+			else {
+				cout << "(False)" << endl;
+				return false;
+			}
+		}
+		else if (strcmp(arguments.at(1), "-d") == 0) {
+			struct stat temp;
+			cout << "testing -d" << endl;
+			if (stat(arguments.at(2), &temp) != 0) {
+//				if (temp.st_mode &S_IFDIR) {
+//					cout << "(True)" << endl;
+//					return true;
+//				}
+				cout << "(False)" << endl;
+				return false;
+			}
+			else if (temp.st_mode &S_IFDIR) {
+				cout << "(True)" << endl;
+				return true;
+			}
+			else {
+				cout << "(False)" << endl;
+				return false;
+			}
+		}
+		else {
+			struct stat temp;
+                        cout << "testing -e" << endl;
+                        if (stat(arguments.at(2), &temp) == 0) {
+                                cout << "(True)" << endl;
+                                return true;
+                        }
+                        else {
+                                cout << "(False)" << endl;
+                                return false;
+                        }
+		}
+	}
+//////////////////////////////////////////////////
         pid_t pid = fork();
         if(pid == 0) {
 	        char* args[arguments.size() + 1];
